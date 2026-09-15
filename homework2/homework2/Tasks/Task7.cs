a3d4c4578eadafccd961782511038823c34e27c0
Игорь <igor1sorokoletov@gmail.com>
2026-09-15 14:17:25 +0300

    complited hw2

---
+++ homework2/homework2/Tasks/Task7.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework2.Tasks;
+
+public class Task7
+{
+    static int ParseAge(string input)
+    {
+        int age = int.Parse(input);
+
+        if (age < 0 || age > 150)
+        {
+            throw new InvalidAgeException(age, "Возраст должен быть в диапазоне от 0 до 150 лет.");
+        }
+
+        return age;
+    }
+}
+
+public class InvalidAgeException : Exception
+{
+    public int Age { get; }
+
+    public InvalidAgeException(int age, string message) : base(message)
+    {
+        Age = age;
+    }
+}
+
+
