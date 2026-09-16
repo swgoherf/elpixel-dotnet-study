0538a50accb6c16d7cf3f6d844419d97bacbf0eb
Игорь <igor1sorokoletov@gmail.com>
2026-09-16 09:38:11 +0300

    refactoring hw3

--- homework3/homework3/Extensions/BookExtensions.cs
+++ homework3/homework3/Extensions/BookExtensions.cs
@@ -3,13 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace homework3.Extensions
+namespace homework3.Extensions;
+
+public static class BookExtensions
 {
-    public static class BookExtensions
+    public static IEnumerable<Book> ByGenre(this IEnumerable<Book> books, string genre)
     {
-        public static IEnumerable<Book> ByGenre(this IEnumerable<Book> books, string genre)
-        {
-            return books.Where(b => b.Genre.Equals(genre, StringComparison.OrdinalIgnoreCase));
-        }
+        return books.Where(b => b.Genre.Equals(genre, StringComparison.OrdinalIgnoreCase));
     }
 }
+
--- homework3/homework3/Models/Book.cs
+++ homework3/homework3/Models/Book.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace homework3.Models
+namespace homework3.Models;
+
+public class Book
 {
-    public class Book
-    {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Genre { get; set; }
-        public int Year { get; set; }
-        public int Pages { get; set; }
-        public bool IsAvailable { get; set; }
-    }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public int Year { get; set; }
+    public int Pages { get; set; }
+    public bool IsAvailable { get; set; }
 }
+
